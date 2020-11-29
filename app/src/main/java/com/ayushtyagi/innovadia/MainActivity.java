d3c7176dc57a41d9930273ba1bfde871f11a1c7d
Ayush <tyagiayush867@gmail.com>
2020-11-29 15:29:52 +0530

    BottomNavigationView

--- app/src/main/java/com/ayushtyagi/innovadia/MainActivity.java
+++ app/src/main/java/com/ayushtyagi/innovadia/MainActivity.java
@@ -1,14 +1,38 @@
 package com.ayushtyagi.innovadia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+        BottomNavigationView bottom_navigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottom_navigation=findViewById(R.id.bottom_navigtion);
+
+        bottom_navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.search_bar:
+
+                        break;
+                        case R.id.home_bar:
+                        break;
+
+                    case R.id.profile_bar:
+                        break;
+
+                }
+
+            }
+        });
     }
 }
